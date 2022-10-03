ad8286df26f9b7b05922bfa9420bcc7ce6a5402d
hugo sanchez <hugo127sanchez@gmail.com>
2022-10-03 09:07:21 -0300

    Inicio Pagina Videojuegos

---
+++ pagina-videojuegos/src/main/java/com/example/grupo/belmg/paginavideojuegos/PaginaVideojuegosApplication.java
@@ -0,0 +1,13 @@
+package com.example.grupo.belmg.paginavideojuegos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginaVideojuegosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginaVideojuegosApplication.class, args);
+	}
+
+}
---
+++ pagina-videojuegos/src/test/java/com/example/grupo/belmg/paginavideojuegos/PaginaVideojuegosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grupo.belmg.paginavideojuegos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginaVideojuegosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
