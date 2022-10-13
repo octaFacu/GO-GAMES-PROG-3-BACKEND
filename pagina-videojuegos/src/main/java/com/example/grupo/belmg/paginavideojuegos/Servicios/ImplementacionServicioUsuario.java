8055ac886d0d036950b11025a9ec0e0732b07a8d
Hugo Sanchez <hugo127sanchez@gmail.com>
2022-10-13 18:50:14 -0300

    Update Encryptacion contraseña
    IMPORTANTE FALTA HACER LA VALIDACION PARA EL LOGIN

--- pagina-videojuegos/src/main/java/com/example/grupo/belmg/paginavideojuegos/Servicios/ImplementacionServicioUsuario.java
+++ pagina-videojuegos/src/main/java/com/example/grupo/belmg/paginavideojuegos/Servicios/ImplementacionServicioUsuario.java
@@ -4,9 +4,11 @@ import com.example.grupo.belmg.paginavideojuegos.Entidades.Categoria;
 import com.example.grupo.belmg.paginavideojuegos.Entidades.Usuario;
 import com.example.grupo.belmg.paginavideojuegos.Repositorios.RepositorioUsuario;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
 
+import javax.transaction.Transactional;
 import java.util.List;
 @Service
 public class ImplementacionServicioUsuario extends ImplementacionServicioBase<Usuario,Long> implements ServicioUsuario{
@@ -14,8 +16,19 @@ public class ImplementacionServicioUsuario extends ImplementacionServicioBase<Us
 
     @Autowired
     RepositorioUsuario repositorio;
+    @Override
+    @Transactional
+    public Usuario save(Usuario entity) throws Exception {
 
+        try{
+            entity.setContraseña(BCrypt.hashpw(entity.getContraseña(),BCrypt.gensalt()));
+            entity = repositorio.save(entity);
+            return entity;
 
+        }catch(Exception e){
+            throw new Exception(e.getMessage());
+        }
+    }
     @Override
     public List<Usuario> find(String filtro) throws Exception {
         try{
