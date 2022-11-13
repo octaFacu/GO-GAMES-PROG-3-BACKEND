fbcf1d550127aa500c066c345d474314a2ef66e1
Octavio <octavio.curadelli@alumnos.frm.utn.edu.ar>
2022-11-13 18:59:12 -0300

    Metodos para controlador imagen

--- pagina-videojuegos/src/main/java/com/example/grupo/belmg/paginavideojuegos/Controladores/ControladorImagen.java
+++ pagina-videojuegos/src/main/java/com/example/grupo/belmg/paginavideojuegos/Controladores/ControladorImagen.java
@@ -3,12 +3,39 @@ package com.example.grupo.belmg.paginavideojuegos.Controladores;
 import com.example.grupo.belmg.paginavideojuegos.Entidades.Imagen;
 import com.example.grupo.belmg.paginavideojuegos.Entidades.Usuario;
 import com.example.grupo.belmg.paginavideojuegos.Servicios.ImplementacionServicioImagen;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping(path = "imagen")
 public class ControladorImagen extends ImplementacionControladorBase<Imagen, ImplementacionServicioImagen>{
+
+    @GetMapping("/searchImagenByVideojuego/{id}")
+    public ResponseEntity<?> findImagenByVideojuegoId(@PathVariable Long id){
+
+        try{
+
+            return ResponseEntity.status(HttpStatus.OK).body(service.findImagenByVideojuegoId(id));
+
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(("{\"error\": \"" + e.getMessage() + "\"}"));
+        }
+
+    }
+
+    @GetMapping("/searchImagenByMerch/{id}")
+    public ResponseEntity<?> findImagenByMerchId(@PathVariable Long id){
+
+        try{
+
+            return ResponseEntity.status(HttpStatus.OK).body(service.findImagenByMerchId(id));
+
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(("{\"error\": \"" + e.getMessage() + "\"}"));
+        }
+
+    }
+
 }
